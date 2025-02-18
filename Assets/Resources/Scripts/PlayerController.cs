ccb43b10d270db4bbfb5237171b2d3a0c25106ae
Ryan <547558@student.fontys.nl>
2025-02-18 10:08:16 +0100

    First commit

---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float maxTransformPostionY;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private string inputAxis = "Vertical"; // Input axis name
+    [SerializeField] private float speed = 5f;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float moveInput = Input.GetAxis(inputAxis);
+        Move(moveInput);
+    }
+
+    public void Move(float moveInput)
+    {
+        Vector2 newPosition = rb.position + Vector2.up * moveInput * speed * Time.deltaTime;
+        // leg new position y uit in je werkwijze
+        newPosition.y = Mathf.Clamp(newPosition.y, -maxTransformPostionY, maxTransformPostionY);
+        rb.MovePosition(newPosition);
+    }
+
+    public void ResetPosition()
+    {
+        rb.position = startPosition;
+    }
+}
